1da132df8896409894ce538a4b8fe814bef0ff3f
Ashvalekh <152001841+Ashvalekh@users.noreply.github.com>
2024-02-03 21:26:08 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+//variables and constant you cant change constants
+const accountId = 4335;
+let accountEmail = "ashva@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur";
+let accountState;
+// accountId = 2
+/* just because of scope we started using Let more often as compare to var
+ block scope and everyhting else*/
+
+accountState = "teribhendifuddi"
+accountEmail = "ashdjfhks@gak.com"
+accountCity = "Bengaluru"
+accountPassword = "212121"
+
+console.log(accountId);
+console.table([accountState,accountEmail, accountId , accountPassword, accountCity])
