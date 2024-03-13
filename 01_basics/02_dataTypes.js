1b9828e9b57751cee142c552da716239a805d305
Ashvalekh <152001841+Ashvalekh@users.noreply.github.com>
2024-03-13 10:31:21 +0000

    sad

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version 
+
+//alert("") this will work in browser better
+
+// make code readable please ashu
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number, bigint, string, boolean, null => standalone, undefined, symbol, 
+
+// whats the type of null 
+console.log(typeof null);
+console.log(typeof undefined);
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,17 +1,7 @@
-//variables and constant you cant change constants
-const accountId = 4335;
-let accountEmail = "ashva@google.com"
-var accountPassword = "12345"
-accountCity = "Jaipur";
-let accountState;
-// accountId = 2
-/* just because of scope we started using Let more often as compare to var
- block scope and everyhting else*/
+const accountEmail = "ash@smtg.com"
+// console.log(accountEmail);
 
-accountState = "teribhendifuddi"
-accountEmail = "ashdjfhks@gak.com"
-accountCity = "Bengaluru"
-accountPassword = "212121"
+let acctCity = "yaad";
+var acctPassword = "humh";
 
-console.log(accountId);
-console.table([accountState,accountEmail, accountId , accountPassword, accountCity])
+console.table([acctPassword, acctCity, accountEmail]);
