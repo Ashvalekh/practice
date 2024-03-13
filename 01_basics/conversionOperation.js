1e25eec3e11d0ad37b2dc354d647a587e31628a3
Ashvalekh <152001841+Ashvalekh@users.noreply.github.com>
2024-03-13 11:39:40 +0000

    as

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,19 @@
+// let score = false
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let LoggedIn = "ash"
+
+let booleanIs = Boolean(LoggedIn)
+console.log(booleanIs);
+
+let someNUmber = 33
+
+let stringNumber = String(someNUmber)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
