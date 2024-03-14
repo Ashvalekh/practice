c60c2ee089425266b7044d956464b936658135c1
Ashvalekh <152001841+Ashvalekh@users.noreply.github.com>
2024-03-14 20:22:39 +0000

    as

---
+++ 01_basics/05_string.js
@@ -0,0 +1,28 @@
+const name = "ashu";
+const repoCount = 50
+
+console.log(name + repoCount + "fh");
+
+console.log(`hello my name is ${name} my repo count is ${repoCount}`);
+
+const gameName = new String('ashu-sh')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0,4)
+
+console.log(newString);
+
+const anotherString = gameName.slice(-5,4)
+console.log(anotherString);
+
+let vame = "   ashu   "
+console.log(vame);
+console.log(vame.trim());
+
