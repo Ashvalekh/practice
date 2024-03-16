5941a2423d71b4a5996fee1ee4cfb80094b810fa
Ashvalekh <152001841+Ashvalekh@users.noreply.github.com>
2024-03-16 19:40:31 +0000

    asd

---
+++ Part2/object.js
@@ -0,0 +1,39 @@
+//++++Objects+++++
+//singleton : apne tarah ka ek hee object jo ki constructor se banega
+//->object.create se singleton bannta h
+
+// object literals
+
+const mySym = Symbol("key1")
+
+const JSuser = {
+    name: "hitesh",
+    "full name": "ashutosh singh rathore",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "jaipur",
+    email: "hitesh@google.com",
+    isLoggedin: false,
+    lastLoginDays: ["mondays", "tuesday"]
+}
+
+// console.log(JSuser["full name"]);
+// console.log(JSuser["email"]);
+// console.log(JSuser[mySym]);
+
+// console.log(mySym);
+
+// JSuser.email = "ashu@cht.com"
+// Object.freeze(JSuser)
+// JSuser.email = "ashu@ttt.com"
+// console.log(JSuser);
+
+JSuser.greeting = function(){
+    console.log("hello user");
+}
+JSuser.greetingTwo = function(){
+    console.log(`hello user, ${this.name}`);
+}
+
+console.log(JSuser.greeting());
+console.log(JSuser.greetingTwo());
