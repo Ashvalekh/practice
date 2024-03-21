f25afef73ec28172e9fbd783f54cb4082d5c3aa1
Ashvalekh <152001841+Ashvalekh@users.noreply.github.com>
2024-03-21 19:05:37 +0000

    as

--- 03Basics/01_functions.js
+++ 03Basics/01_functions.js
@@ -1,36 +1,12 @@
-
-
-// function sayMyName() {
-//   console.log("A");
-//   console.log("S");
-//   console.log("H");
-//   console.log("U");
-// }
-
-// sayMyName() // "sayMyName" will be reference and if i want to execute it use parentheis"()" 
-
-// const addition  = function addTwoNumbers(number1, number2){
-//     return number1 + number2;
-// }
-
-// let add = addition(3, 4);
-// console.log(add);
-
-function addTwoNumbers(number1, number2){
-    // console.log(number1 + number2);
+function addTwoNumbers(A, B) {
+//   let result = A + B;
+//   //  console.log(`Result is : ${result}`);
+//   return result;
+return A+B
 }
 
-addTwoNumbers(4, "5")
+result = addTwoNumbers(3, 5);
 
+console.log(`Result : ${result}`);
 
-function loginUserMessage(username){
-    // toh in case kissi ne naam likha hee nhi then???
-    if (username === undefined) {
-        console.log("please enter a username");// output k saath ara h undefined just looged in to remove that use return here
-        return
-    }
-    return `${username} just logged in `
-}
-
-console.log(loginUserMessage())
-
+// or kittae t
