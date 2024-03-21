f546e7f41d189c0ad8c3fbb7f984d8166b43d2ee
Ashvalekh <152001841+Ashvalekh@users.noreply.github.com>
2024-03-21 18:15:38 +0000

    as

---
+++ Part2/areaofrec.js
@@ -0,0 +1,14 @@
+// Area of rectangle
+// let length = 5;
+// let width = 6;
+// const Area = length * width;
+// console.log(`are of rectangle is ${Area}`);
+
+function areaofrectangle(length, width) {
+    return (length*width)
+}
+
+let length = 4;
+let width = 5;
+let area = areaofrectangle(length,width)
+console.log(area);
