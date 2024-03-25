e6960c257ac973a953f363082925f9a258c4d3be
Ashvalekh <152001841+Ashvalekh@users.noreply.github.com>
2024-03-25 21:31:57 +0000

    fg

--- 04_controlflow/truthy.js
+++ 04_controlflow/truthy.js
@@ -21,3 +21,21 @@ const emptyObj = {}
 if (Object.keys(emptyObj).length ===0){
     console.log("object is empty");
 }
+
+//NUllish coalescing operator (??): null undefined ye seerf null or undefined k liye bana h 
+let val1;
+// val1 = 5??10
+// val1 = null??10
+// val1 = undefined??16
+
+val1 = null??13??45
+
+console.log(val1);
+
+//ternary operator
+
+// condition ? true:false
+
+const iceTeaPrice = 100
+
+iceTeaPrice <= 80? console.log("less than"): console.log("more than 80");
