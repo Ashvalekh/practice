ace72804e90c287d6a0bbf82b0de3641319d9927
Ashvalekh <152001841+Ashvalekh@users.noreply.github.com>
2024-03-28 19:17:04 +0000

    asd

---
+++ 05_iterations/forIn.js
@@ -0,0 +1,17 @@
+// objects pr loop kaise lagaye caz forof toh lagta nhi h 
+const myObject = {
+    js: "javascript",
+    cpp: "C++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programing = ["js","rb", "py", "java", "cpp"]
+
+for (const key in programing) {
+//   console.log(programing[key]);
+}
